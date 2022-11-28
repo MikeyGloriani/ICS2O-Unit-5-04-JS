72f37ef899c2a9cab1e913b3948e111848c0a463
Mikey Gloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-11-27 21:32:16 -0500

    init

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function myButtonClicked(accessGranted) {
     day == "Tuesday" || day == "Thursday" || (age > 12 && age < 21)
 
   //process
-  if (accessGranted == true) {
+  if (accessGranted = true ) {
     document.getElementById("entry").innerHTML =
       "<p>You can enter the museum</p>"
   } else {
